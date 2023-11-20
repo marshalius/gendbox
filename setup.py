aff8d87a5567635220924d88cc00b7c166f19479
Candaş Koru <candas.koru@gmail.com>
2023-11-20 15:22:47 +0300

    modules restructured

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name='gendbox',
-    version='0.1.0',
+    version='0.1.2',
     description='Açıklama',
     author='Candaş Koru',
-    packages=find_packages(exclude=['learning']),
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'pandas'
     ],
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
 )
