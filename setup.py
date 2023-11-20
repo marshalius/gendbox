de854ba5724470d57421efdd87ae8ec55d4d07bb
Candaş Koru <candas.koru@gmail.com>
2023-11-20 13:08:24 +0300

    Birth of the Gendbox

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mypackage',
+    version='0.1.0',
+    description='Açıklama',
+    author='Your Name',
+    packages=['mypackage'],
+    install_requires=[
+        'numpy',
+        'pandas'
+    ],
+)
