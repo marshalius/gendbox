38f0905336901a313e0fdcf27c172e9a24466858
Candaş Koru <candas.koru@gmail.com>
2023-11-22 17:35:19 +0300

    utils added

--- build/lib/gendbox/preprocessing/normalization.py
+++ build/lib/gendbox/preprocessing/normalization.py
@@ -5,6 +5,12 @@ class MinMax:
     
     def fit(self, data):
         try:
+            if str(type(data)) == "<class 'pandas.core.frame.DataFrame'>":
+                data = data.values.tolist()
+            if(str(type(data)) == "<class 'pandas.core.series.Series'>" or 
+               str(type(data)) == "<class 'numpy.matrix'>" or 
+               str(type(data)) == "<class 'numpy.ndarray'>"):
+                data = data.tolist()
             self.min_value = min(data)
             self.max_value = max(data)
         except Exception as e:
@@ -12,11 +18,23 @@ class MinMax:
     
     def transform(self, data):
         try:
-            new_list = []
-            for value in data:
-                normalized_value = (value - self.min_value) / (self.max_value - self.min_value)
-                new_list.append(normalized_value)
-            return new_list
+            from gendbox.utils import _DataConverter, _is_matrix
+            conv = _DataConverter(data)
+            data = conv._convert_to_list()
+            new_data = []
+            if _is_matrix(data):
+                for i in range(0, len(data)):
+                    new_row = []
+                    for value in range(0, len(data[0])):
+                        normalized_value = (value - self.min_value) / (self.max_value - self.min_value)
+                        new_row.append(normalized_value)
+                    new_data.append(new_row)
+            else:
+                for value in data:
+                    normalized_value = (value - self.min_value) / (self.max_value - self.min_value)
+                    new_data.append(normalized_value)
+            new_data = conv._unconvert(new_data)
+            return new_data
         except Exception as e:
             print(f'An unexcepted error has occured: {e}')
     
